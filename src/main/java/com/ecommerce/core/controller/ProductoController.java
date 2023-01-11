c31c1088593920662b8ba5638e3298ab65412db6
razhe <gustavo.acu.rivas@gmail.com>
2023-01-11 15:02:50 -0300

    funcionalidad editar y eliminar con sus respectivas vistas

--- src/main/java/com/ecommerce/core/controller/ProductoController.java
+++ src/main/java/com/ecommerce/core/controller/ProductoController.java
@@ -1,9 +1,13 @@
 package com.ecommerce.core.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,6 +41,28 @@ public class ProductoController {
 		
 		producto.setUsuario(usr);
 		ps.guardarProducto(producto);
+		return "redirect:/productos";
+	}
+	
+	@GetMapping(path = {"/editar/{id}"})
+	public ModelAndView editarProducto(@PathVariable Long id) {
+		Producto producto = new Producto();
+		Optional<Producto> optional = ps.obtenerProducto(id);
+		producto = optional.get();
+		LOGGER.info(producto.toString());
+		return new ModelAndView("productos/edit").addObject("producto", producto);
+	}
+	
+	@PostMapping(path = {"/actualizar"})
+	public String actualizarProducto(@ModelAttribute Producto producto) {
+		//model attribute solamente si estamos usando th:object="${productos}"
+		ps.actualizarProducto(producto);
+		return "redirect:/productos";
+	}
+	
+	@GetMapping(path = {"/eliminar/{id}"})
+	public String eliminarProducto(@PathVariable Long id) {
+		ps.borrarProducto(id);
 		return "redirect:/productos";
 	}
 }
